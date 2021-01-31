eadac31720c541b8c5aa0c9377c4e78f188cf3f3
cicekh <haydarcicekci@hotmail.com>
2021-01-31 10:33:11 +0300

    first commit

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        // Bir iş sınıfı başka kesinlikle newlemez.
+
+        InMemoryProductDal _productDal;
+
+        public ProductManager(InMemoryProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            // İş kodları varsa yazıyoruz.
+            // Yetkisi var mı?
+            // Başka kural varsa onlara bak?
+            return _productDal.GetAll();
+        }
+    }
+}
